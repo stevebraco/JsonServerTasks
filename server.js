68b53332b37db610211396b5f8e21e2b0ada632e
steve <steve.braco@gmail.com>
2022-10-23 22:54:59 +0200

    add server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+console.log(port);
 
 server.use(middlewares);
 server.use(router);
